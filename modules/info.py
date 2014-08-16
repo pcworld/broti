66d387e1379ad6c60923c92de4b7d2396ac8d87c
Stefan <misc@stefan-koch.name>
2014-08-16 11:10:22 +0200

    added docs for polling

--- modules/info.py
+++ modules/info.py
@@ -1,5 +1,5 @@
 def overview(bot, replyto, user, args):
-    bot.msg(replyto, 'Commands: *notify, *rage')
+    bot.msg(replyto, 'Commands: *notify, *poll (*vote), *rage')
 
 def load_module(bot):
     bot.hook_command('help', overview)
