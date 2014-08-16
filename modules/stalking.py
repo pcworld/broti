0944db6f3b1977c9b24245bcbd631fd55aa2bfb3
Stefan <misc@stefan-koch.name>
2014-08-16 10:33:38 +0200

    fixed stalking module for events api

--- modules/stalking.py
+++ modules/stalking.py
@@ -1,5 +1,17 @@
 import time
 
+def add_joined(bot, replyto, username):
+    cur = bot.factory.db.cursor()
+    cur.execute('''INSERT INTO stalking (username, date, action)
+        VALUES (?, ?, ?)''', (username, time.time(), 'join'))
+    bot.factory.db.commit()
+    
+def add_left(bot, replyto, username):
+    cur = bot.factory.db.cursor()
+    cur.execute('''INSERT INTO stalking (username, date, action)
+        VALUES (?, ?, ?)''', (username, time.time(), 'leave'))
+    bot.factory.db.commit()
+
 def load_module(bot):
     cur = bot.factory.db.cursor()
     bot.factory.db.execute('''CREATE TABLE IF NOT EXISTS stalking (
@@ -9,14 +21,5 @@ def load_module(bot):
         )''')
     bot.factory.db.commit()
 
-def add_joined(bot, username):
-    cur = bot.factory.db.cursor()
-    cur.execute('''INSERT INTO stalking (username, date, action)
-        VALUES (?, ?, ?)''', (username, time.time(), 'join'))
-    bot.factory.db.commit()
-    
-def add_left(bot, username):
-    cur = bot.factory.db.cursor()
-    cur.execute('''INSERT INTO stalking (username, date, action)
-        VALUES (?, ?, ?)''', (username, time.time(), 'leave'))
-    bot.factory.db.commit()
+    bot.hook_action('userJoined', add_joined)
+    bot.hook_action('userLeft', add_left)
