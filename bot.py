0545d9129898483f6bdc525b3facc63e46f048fa
Stefan Koch <programming@stefan-koch.name>
2014-08-20 22:22:32 +0200

    fixed bug

--- bot.py
+++ bot.py
@@ -58,8 +58,8 @@ class Bot(irc.bot.SingleServerIRCBot):
             user, _, host = e.source.partition('!')
             c.privmsg(user, msg)
 
-    def user_online(user, bot):
-        for channel in bot.channels.values():
+    def user_online(self. user):
+        for channel in self.channels.values():
             if channel.has_user(user):
                 return True
 
